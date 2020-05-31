0a828ea6d797f7aebd9544230b3dcc6d6e004a79
ripee <ripee.dev@gmail.com>
2020-05-31 20:17:07 +0200

    Havoc-ify

--- hidl/fastcharge/Android.mk
+++ hidl/fastcharge/Android.mk
@@ -31,8 +31,7 @@ LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libhidlbase \
     libhidltransport \
-    libutils \
-    vendor.lineage.fastcharge@1.0
+    libutils
 
 LOCAL_MODULE := vendor.lineage.fastcharge@1.0-service.samsung
 LOCAL_INIT_RC := vendor.lineage.fastcharge@1.0-service.samsung.rc
